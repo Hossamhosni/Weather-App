fdf578a892534b0ec5212b045d3c2f617e4f2ad2
Hossam <jkasdhkjadas>
2017-01-22 23:03:01 +0200

    used codes instead of weather descriptions to get Icons

--- script.js
+++ script.js
@@ -8,30 +8,35 @@
 			headers: {
 				"X-Mashape-Key": "ASlvvienCDmshcxWF6e0ztOgSDN1p1FpF81jsn1TK5LnWSHKt6"
 			},
-			data:"lat="+ lat +"&lng="+lon,
+			data:"lat="+ lat +"&lng="+ lon,
 			success:function(data) {
-				var data = JSON.parse(data);
-				tempCelsius = data.query.results.channel.item.condition.temp;
-				tempFahrenheit = Math.round(tempCelsius * 9/5 + 32);
-				humidity = data.query.results.channel.atmosphere.humidity;
-				description = data.query.results.channel.item.condition.text;
-				var icon = getThermometerIcon(tempCelsius);
-				var hours = new Date(new Date().getTime()).toLocaleTimeString().split(" ")[0].split(":")[0];
-				var minutes = new Date(new Date().getTime()).toLocaleTimeString().split(" ")[0].split(":")[1];
-				var seconds = new Date(new Date().getTime()).toLocaleTimeString().split(" ")[0].split(":")[2];
-				console.log(hours,minutes,seconds);
-				var data = new Date();
-				console.log(data);
-				$Weather.append("<i class='fa " +  icon + " fa-2x' aria-hidden='true'></i>")
-				$Weather.append("<h2 id = 'temp'> " + tempCelsius + " &deg; <span id = 'cOrF'> C</span> </h2>");
-				$Weather.append("<i class='fa fa-toggle-on fa-2x' id='toggle-on' aria-hidden='true'></i>")
-				$Weather.append("<i class='fa fa-toggle-off fa-2x' id = 'toggle-off' aria-hidden='true'></i>");
-				$Weather.append("<h2 id= 'description'>" + description + "</h2>");
-				$Weather.append("<i class='owf " + getWeatherIcon(description) +" owf-5x'></i>");
-				$Weather.append("<h2 id='time'></h2>" )
-				document.getElementById("reload").style.display = "none";
-				clicks();
-				startTime();
+				try {
+					var data = JSON.parse(data);
+					var conditionCode = Number(data.query.results.channel.item.condition.code);
+					tempCelsius = data.query.results.channel.item.condition.temp;
+					tempFahrenheit = Math.round(tempCelsius * 9/5 + 32);
+					humidity = data.query.results.channel.atmosphere.humidity;
+					description = data.query.results.channel.item.condition.text;
+					var icon = getThermometerIcon(tempCelsius);
+					$Weather.append("<i class='fa " +  icon + " fa-2x' aria-hidden='true'></i>")
+					$Weather.append("<h2 id = 'temp'> " + tempCelsius + " &deg; <span id = 'cOrF'> C</span> </h2>");
+					$Weather.append("<i class='fa fa-toggle-on fa-2x' id='toggle-on' aria-hidden='true'></i>")
+					$Weather.append("<i class='fa fa-toggle-off fa-2x' id = 'toggle-off' aria-hidden='true'></i>");
+					$Weather.append("<h2 id= 'description'>" + description + "</h2>");
+					$Weather.append("<i class='owf " + getWeatherIcon(conditionCode) +" owf-5x'></i>");
+					$Weather.append("<h2 id='time'></h2>" )
+					document.getElementById("reload").style.display = "none";
+					clicks();
+					startTime();
+				}
+				catch (e) {
+					$Weather.append("<h2 id= 'description'>" + "Sorry, Couldn't find your Weather!" + "</h2>");
+					$Weather.append("<i class='fa fa-meh-o fa-4x' aria-hidden='true'></i>")
+					document.getElementById("reload").style.display = "none";
+				};
+			} ,
+			Error: function() {
+				$Weather.append("<h2 id= 'description'>" + "Sorry, Couldn't find your data" + "</h2>");
 			}
 		});
 	}
@@ -44,9 +49,18 @@
     var s = today.getSeconds();
     m = checkTime(m);
     s = checkTime(s);
-    document.getElementById('time').innerHTML =
-    h + ":" + m + ":" + s;
-    var t = setTimeout(startTime, 500);
+		if (h > 11)
+		{
+			h -= 12;
+			h = checkTime(h);
+			document.getElementById('time').innerHTML =
+	    h + ":" + m + ":" + s + " PM";
+		}
+		else {
+	    document.getElementById('time').innerHTML =
+	    h + ":" + m + ":" + s + " AM";
+		}
+    setTimeout(startTime, 1000);
 	}
 	function checkTime(i) {
 	    if (i < 10) {i = "0" + i};
@@ -70,7 +84,7 @@
 		document.getElementById("toggle-on").addEventListener("click",function() {
 			document.getElementById("toggle-on").style.display = "none";
 			document.getElementById("toggle-off").style.display = "inline";
-			document.getElementById("temp").innerHTML = " " + tempFahrenheit + "&deg; " + "<span id='cOrF'> F</span>";
+			document.getElementById("temp").innerHTML = " " + tempFahrenheit + " &deg; " + "<span id='cOrF'> F </span>";
 		});
 		document.getElementById("toggle-off").addEventListener("click",function() {
 			document.getElementById("toggle-off").style.display = "none";
@@ -94,39 +108,150 @@
 			return "fa-thermometer-full";
 		}
 	};
-	var getWeatherIcon = function (description) {
+
+	//This Function will get the Weather Icon based on the weather description
+	var getWeatherIcon = function (conditionCode) {
 		var dayOrNight;
 		if ((new Date()).getHours() > 5 && (new Date()).getHours() < 18) {
 			dayOrNight = "d";
 		} else {
 			dayOrNight = "n";
 		}
-		if (dayOrNight == "d") {
-			document.querySelector("body").style.backgroundColor = "#4174eb";
-		} else {
-			document.querySelector("body").style.backgroundColor = "#060606";
-		}
-		switch (description) {
-			case "Mostly Cloudy" :
-			case "Cloudy":
-				return "owf-804-n";
+		switch (conditionCode) {
+			case	0	:
+				return "owf-900";
 				break;
-			case "Sunny":
-			case "Clear":
-			case "Fair" :
-				return "owf-800-" + dayOrNight;
+			case	1	:
+			 return "owf-901";
+			 break;
+			case	2	:
+				return "owf-902";
 				break;
-			case "Showers":
-					return "owf-522";
-					break;
-			case "Rain":
-				return "owf-520";
+			case	3	:
+			  return "owf-212";
+			  break;
+			case	4	:
+				return "owf-211";
 				break;
-			case "Partly Cloudy" :
-				return "owf-801-" + dayOrNight;
+			case	5	:
+				return "owf-616";
 				break;
-			case "Blustery" :
-				return "owf-957";
+			case	6	:
+			case	7	:
+				return "owf-612";
+				break;
+			case	8	:
+			case	9	:
+				return "owf-302";
+				break;
+			case	10:
+				return "owf-511";
+				break;
+			case	11:
+			case	12:
+				return "owf-521";
+				break;
+			case	13:
+				return "owf";
+				break;
+			case	14:
+				return "owf";
+				break;
+			case	15:
+				 return "owf";
+				 break;
+			case	16:
+				return "owf";
+				break;
+			case	17:
+				return "owf";
+				break;
+			case	18:
+				return "owf";
+				break;
+			case	19:
+				return "owf";
+				break;
+			case	20:
+				return "owf";
+				break;
+			case	21:
+				return "owf";
+				break;
+			case	22:
+				return "owf";
+				break;
+			case	23:
+				return "owf";
+				break;
+			case	24:
+				return "owf";
+				break;
+			case	25:
+				return "owf";
+				break;
+			case	26:
+				return "owf-804";
+				break;
+			case	27:
+				return "owf";
+				break;
+			case	28:
+				return "owf";
+				break;
+			case	29:
+				return "owf";
+				break;
+			case	30:
+				return "owf";
+				break;
+			case	31:
+				return "owf";
+				break;
+			case	32:
+			case	36:
+				return "owf";
+				break;
+			case	33:
+				return "owf";
+				break;
+			case	34:
+				return "owf";
+				break;
+			case	35:
+				return "owf";
+				break;
+			case	37:
+			case	38:
+			case	39:
+				return "owf";
+			case	40:
+			return "owf";
+			break;
+			case	41:
+				return "owf";
+				break;
+			case	42:
+				return "owf";
+				break;
+			case	43:
+				return "owf";
+				break;
+			case	44:
+			return "owf";
+			break;
+			case	45:
+				return "owf";
+				break;
+			case	46:
+				return "owf";
+				break;
+			case	47:
+				return "owf";
+				break;
+			case	3200:
+				return "owf";
+				break;
 		}
 	};
 
